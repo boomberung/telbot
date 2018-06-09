87b91236b413babdc77605d5801e07b0c7f66af9
Гараев Никита <32163192+boomberung@users.noreply.github.com>
2018-06-09 20:30:01 +0300

    wqe

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
 import requests
 
-r = requests.get("https://api.telegram.org/bot286888171:AAGb0YLpt_aXbeI3DvjTz7TlASwO0nq49-8/getUpdates")
-print(r.text)
+r = requests.post("https://api.telegram.org/bot286888171:AAGb0YLpt_aXbeI3DvjTz7TlASwO0nq49-8/getUpdates")
